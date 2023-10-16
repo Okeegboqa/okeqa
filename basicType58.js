d868b2c88b4c298ee433937b284997669b6f2678
Oke Egbo <Okechukwu.Egbo@symphony-solutions.com>
2023-10-16 08:59:50 +0100

    add-homework

---
+++ basicType58.js
@@ -0,0 +1,7 @@
+const myVarsArray = ["var_1", "var_2", "var_3", "var_4"];
+
+// Accessing the second element (index 1) and assigning it to myVar
+const myVar = myVarsArray[1];  
+
+// Outputs: "var_2"
+console.log(myVar);  
