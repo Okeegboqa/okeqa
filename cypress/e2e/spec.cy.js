0e3534b108dbb982a806c7dc5e1703906b21c4ae
Oke Egbo <Okechukwu.Egbo@symphony-solutions.com>
2023-11-01 15:24:12 +0100

    cypresssui_assignment

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,35 @@
+describe('template spec', () => {
+  it('passes', () => {
+
+    // Visit URL
+    cy.visit('http://localhost:3000/signin');
+
+    // Enter Username and Password
+    cy.get('#username').type('Dennisoke');
+    cy.get('#password').type('Dennisme1*');
+
+    // Click on Login Btn
+    cy.get('[data-test="signin-submit"]').click();
+
+    // Click Next to Set Up account
+    cy.get('[data-test="user-onboarding-next"] > .MuiButton-label').click();
+
+    // Create Bank Account
+
+    // Enter Bank Name
+    cy.get('#bankaccount-bankName-input').type('Big Bank');
+
+    // Enter Routing Number
+    cy.get('#bankaccount-routingNumber-input').type('123452323');
+
+    // Enter Account Number
+    cy.get('#bankaccount-accountNumber-input').type('9876543210');
+    
+    // Click on Submit
+    cy.get('[data-test="bankaccount-submit"]').click();
+
+    // Click on Done
+    cy.get('[data-test="user-onboarding-next"] > .MuiButton-label').click();
+  })
+})
+
